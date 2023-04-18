8cb4e9b7b0b870ca8eabb6f343ec954021de9f1e
Zotigh <lzotigh1@cnm.edu>
2023-04-17 22:39:16 -0600

    Added some default fields to populate list.

--- DemoAPI/Data/CouponStore.cs
+++ DemoAPI/Data/CouponStore.cs
@@ -4,6 +4,10 @@ namespace DemoAPI.Data
 {
     public class CouponStore
     {
-        public static List<Coupon> couponList = new List<Coupon>();
+        public static List<Coupon> couponList = new List<Coupon>()
+        {
+            new Coupon { Id = 1, Name = "10OFF", Percent = 10, IsActive = true },
+            new Coupon { Id = 2, Name = "20OFF", Percent = 20, IsActive = false }
+        };
     }
 }
