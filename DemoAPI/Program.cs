d1d7168749d59891093998169e2aeb0c4d3a7233
Zotigh <lzotigh1@cnm.edu>
2023-04-17 22:57:01 -0600

    Modified comment to provided a better explanation.

--- DemoAPI/Program.cs
+++ DemoAPI/Program.cs
@@ -70,7 +70,7 @@ app.MapGet("/api/coupon", () => {
     return Results.Ok(CouponStore.couponList);
 });
 
-//This MapGet function returns the coupon with the specific ID.
+//This MapGet function returns the coupon with the request specific ID when ran and requested.
 app.MapGet("/api/coupon/{id:int}", (int id) => {
     return Results.Ok(CouponStore.couponList.FirstOrDefault(u=>u.Id==id));
 });
