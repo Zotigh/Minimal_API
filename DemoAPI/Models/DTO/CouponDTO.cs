4eea3450b8dd6c1c7392fef6f85b28a18d3436c1
Zotigh <lzotigh1@cnm.edu>
2023-04-23 00:36:32 -0600

    Added the desired fields to the couponDTO class.

--- DemoAPI/Models/DTO/CouponDTO.cs
+++ DemoAPI/Models/DTO/CouponDTO.cs
@@ -2,5 +2,15 @@
 {
     public class CouponDTO
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Percent { get; set; }
+
+        public bool IsActive { get; set; }
+
+        //The "?" tells the compiler that this feild is nullable which should satisfy the compiler since it knows.
+        public DateTime? Created { get; set; }
     }
 }
