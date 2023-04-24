4b9394bd2bb33f58c9116c7c216f02a1e835fa3f
Zotigh <lzotigh1@cnm.edu>
2023-04-23 23:43:26 -0600

    Documentation.

--- DemoAPI/MappingConfig.cs
+++ DemoAPI/MappingConfig.cs
@@ -8,6 +8,8 @@ namespace DemoAPI
     {
         public MappingConfig() 
         {
+            //This mapps the coupon to CouponCreateDTO. The Reverse map allows the Coupon object
+            //to be mapped to another class.
             CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
             CreateMap<Coupon, CouponDTO>().ReverseMap();        
         }
