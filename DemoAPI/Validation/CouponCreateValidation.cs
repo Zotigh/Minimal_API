1728b686f3d1debd7c626ec11448d2d2957e1a07
Zotigh <lzotigh1@cnm.edu>
2023-04-25 10:51:27 -0600

    Documentation.

--- DemoAPI/Validation/CouponCreateValidation.cs
+++ DemoAPI/Validation/CouponCreateValidation.cs
@@ -4,6 +4,7 @@ using DemoAPI.Models.DTO;
 
 namespace DemoAPI.Validation
 {
+    // defines what this class is validating by using AbstractValidator and the class.
     public class CouponCreateValidation : AbstractValidator<CouponCreateDTO>
     {
 
