76652b33eefba68539e0ccf0ac7e17c43f2b13ce
Zotigh <lzotigh1@cnm.edu>
2023-04-25 11:21:56 -0600

    Added variables that we want to use for the response.

--- DemoAPI/Models/APIResponse.cs
+++ DemoAPI/Models/APIResponse.cs
@@ -1,6 +1,13 @@
-﻿namespace DemoAPI.Models
+﻿using System.Net;
+
+namespace DemoAPI.Models
 {
     public class APIResponse
     {
+        public bool IsSuccess { get; set; }
+
+        public Object Result { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string> ErrorMessages { get; set; }
     }
 }
