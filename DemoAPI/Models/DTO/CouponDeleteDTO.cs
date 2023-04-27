eb99d304db31ef6eff3d8e7541caf6b18020f0fa
Zotigh <lzotigh1@cnm.edu>
2023-04-26 23:28:59 -0600

    Created a DTO to use to delete a column/object.

---
+++ DemoAPI/Models/DTO/CouponDeleteDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAPI.Models.DTO
+{
+    public class CouponDeleteDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Percent { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
