76281463873f38944d25abb33292627272bc7933
Zotigh <lzotigh1@cnm.edu>
2023-04-26 23:29:28 -0600

    Added the variables for the delete DTO.

--- DemoAPI/Models/DTO/CouponDeleteDTO.cs
+++ DemoAPI/Models/DTO/CouponDeleteDTO.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public int Percent { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set;}
     }
 }
