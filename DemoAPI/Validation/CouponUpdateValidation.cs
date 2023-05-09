86c7dbfbff5baebedb0c32fff90d9b5d79f46359
Zotigh <lzotigh1@cnm.edu>
2023-05-08 23:14:53 -0600

    Added the update vaidator.

---
+++ DemoAPI/Validation/CouponUpdateValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+//using DemoAPI.Models;
+using DemoAPI.Models.DTO;
+
+namespace DemoAPI.Validation
+{
+    // defines what this class is validating by using AbstractValidator and the class.
+    public class CouponCreateValidation : AbstractValidator<CouponCreateDTO>
+    {
+        //Validation constructor
+        public CouponCreateValidation()
+        {
+            //Defines what the rules are for.
+
+            //Defines rule will not be empty.
+            RuleFor(model => model.Name).NotEmpty();
+
+           //Defines that the percent must be between a certain threshold 1-100.
+            RuleFor(model => model.Percent).InclusiveBetween(1, 100);
+        }
+    }
+}
