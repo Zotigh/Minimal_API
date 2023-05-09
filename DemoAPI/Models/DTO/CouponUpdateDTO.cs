728adbf2cd048c0dee36a1237601a9840c2b2a8d
Zotigh <lzotigh1@cnm.edu>
2023-05-08 23:11:07 -0600

    Added methods that will be updated in the Update DTO.

--- DemoAPI/Models/DTO/CouponUpdateDTO.cs
+++ DemoAPI/Models/DTO/CouponUpdateDTO.cs
@@ -2,5 +2,9 @@
 {
     public class CouponUpdateDTO
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Percent { get; set; }
+        public bool IsActive { get; set; }
     }
 }
